597a9ca7b606ef74192dbce6ecd7a5f9aff1fac4
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-07-10 19:01:17 -0300

    ADD scanterra mods and products from branch 7

--- product_scanterra_extension/__init__.py
+++ product_scanterra_extension/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-# Sistemas ADHOC - ADHOC SA
-# https://launchpad.net/~sistemas-adhoc
+#    Ingenieria ADHOC - ADHOC SA
+#    https://launchpad.net/~ingenieria-adhoc
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- product_scanterra_extension/sale.py
+++ product_scanterra_extension/sale.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Sistemas ADHOC - ADHOC SA
-#    https://launchpad.net/~sistemas-adhoc
+#    Ingenieria ADHOC - ADHOC SA
+#    https://launchpad.net/~ingenieria-adhoc
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -26,6 +26,23 @@ from openerp.tools.translate import _
 class sale_order_line(osv.osv):
     _inherit = "sale.order.line"
 
+# Ahora no lo concatenamos porque lo agreagamos al reporte directamente, hacemos esto nuevo
+    # def product_id_change(self, cr, uid, ids, pricelist, product, qty=0,
+    #         uom=False, qty_uos=0, uos=False, name='', partner_id=False,
+    #         lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False, context=None):
+    #     res = super(sale_order_line, self).product_id_change(cr, uid, ids, pricelist, product, qty,
+    #         uom, qty_uos, uos, name, partner_id,
+    #         lang, update_tax, date_order, packaging, fiscal_position, flag, context)
+    #     partner_obj = self.pool.get('res.partner')
+    #     lang = partner_obj.browse(cr, uid, partner_id).lang
+    #     context_partner = {'lang': lang, 'partner_id': partner_id}
+    #     product_obj = self.pool.get('product.product')
+    #     if product:
+    #         product_obj = product_obj.browse(cr, uid, product, context=context_partner)        
+    #         if not flag and product_obj.categ_id.name and res['value']['name']:
+    #             res['value']['name'] = product_obj.categ_id.name + ' - '+ res['value']['name']
+    #     return res
+
     def product_id_change(self, cr, uid, ids, pricelist, product, qty=0,
             uom=False, qty_uos=0, uos=False, name='', partner_id=False,
             lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False, context=None):
@@ -36,10 +53,19 @@ class sale_order_line(osv.osv):
         lang = partner_obj.browse(cr, uid, partner_id).lang
         context_partner = {'lang': lang, 'partner_id': partner_id}
         product_obj = self.pool.get('product.product')
+        attachment_obj = self.pool['ir.attachment']
         if product:
-            product_obj = product_obj.browse(cr, uid, product, context=context_partner)        
-            if not flag and product_obj.categ_id.name and res['value']['name']:
-                res['value']['name'] = product_obj.categ_id.name + ' - '+ res['value']['name']
-        return res
+            if not flag:
+                product_br = product_obj.browse(cr, uid, product, context=context_partner)        
+                # Si hay desciprcion de venta, solo llevamos esta (sin nombre de producto)
+                if product_br.description_sale:
+                    res['value']['name'] = product_br.description_sale
+                else:
+                    res['value']['name'] = product_obj.name_get(cr, uid, [product_br.id], context=context_partner)[0][1]
+                attachment_ids = attachment_obj.search(cr, uid, [('res_model','=','product.product'),('res_id','=',product)], context=context)
+                if attachment_ids:
+                    attachemnt_desc = ', '.join([at.name for at in attachment_obj.browse(cr, uid, attachment_ids, context=context)])
+                    res['value']['name'] += '\n' + ('Ver adjuntos: ') + attachemnt_desc                                  
+        return res    
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
