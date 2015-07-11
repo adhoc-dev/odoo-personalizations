4cc04da4f8c308f43608ed9276ad1de75d7290b0
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-07-11 11:06:29 -0300

    IMP migrate scanterra extension to v8

--- product_scanterra_extension/product.py
+++ product_scanterra_extension/product.py
@@ -1,27 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Ingenieria ADHOC - ADHOC SA
-#    https://launchpad.net/~ingenieria-adhoc
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+from openerp.osv import fields, osv
 
-from openerp import netsvc
-from openerp.osv import fields, osv, orm
-from openerp.tools.translate import _
 
 class product_segment(osv.osv):
     _name = "product.segment"
@@ -29,13 +8,14 @@ class product_segment(osv.osv):
     _order = "name"
 
     _constraints = [
-        (osv.osv._check_recursion, 'Error ! You cannot create recursive categories.', ['parent_id'])
+        (osv.osv._check_recursion,
+            'Error ! You cannot create recursive categories.', ['parent_id'])
     ]
 
     def name_get(self, cr, uid, ids, context=None):
         if not len(ids):
             return []
-        reads = self.read(cr, uid, ids, ['name','parent_id'], context=context)
+        reads = self.read(cr, uid, ids, ['name', 'parent_id'], context=context)
         res = []
         for record in reads:
             name = record['name']
@@ -49,11 +29,17 @@ class product_segment(osv.osv):
         return dict(res)
 
     _columns = {
-        'name': fields.char('Name', required=True, translate=True),
-        'complete_name': fields.function(_name_get_fnc, type="char", string='Name'),
-        'parent_id': fields.many2one('product.segment','Parent Segment', select=True),
-        'child_id': fields.one2many('product.segment', 'parent_id', string='Children Segments'),
-        'sequence': fields.integer('Sequence', help="Gives the sequence order when displaying a list of product segments."),        
+        'name': fields.char(
+            'Name', required=True, translate=True),
+        'complete_name': fields.function(
+            _name_get_fnc, type="char", string='Name'),
+        'parent_id': fields.many2one(
+            'product.segment', 'Parent Segment', select=True),
+        'child_id': fields.one2many(
+            'product.segment', 'parent_id', string='Children Segments'),
+        'sequence': fields.integer(
+            'Sequence',
+            help="Gives the sequence order when displaying a list of product segments."),        
     }
 
 
@@ -63,13 +49,14 @@ class product_type(osv.osv):
     _order = "name"
 
     _constraints = [
-        (osv.osv._check_recursion, 'Error ! You cannot create recursive categories.', ['parent_id'])
+        (osv.osv._check_recursion,
+            'Error ! You cannot create recursive categories.', ['parent_id'])
     ]
 
     def name_get(self, cr, uid, ids, context=None):
         if not len(ids):
             return []
-        reads = self.read(cr, uid, ids, ['name','parent_id'], context=context)
+        reads = self.read(cr, uid, ids, ['name', 'parent_id'], context=context)
         res = []
         for record in reads:
             name = record['name']
@@ -83,20 +70,27 @@ class product_type(osv.osv):
         return dict(res)
 
     _columns = {
-        'name': fields.char('Name', required=True, translate=True),
-        'complete_name': fields.function(_name_get_fnc, type="char", string='Name'),
-        'parent_id': fields.many2one('product.type','Parent Type', select=True),
-        'child_id': fields.one2many('product.type', 'parent_id', string='Children Type'),
-        'sequence': fields.integer('Sequence', help="Gives the sequence order when displaying a list of product Types."),        
+        'name': fields.char(
+            'Name', required=True, translate=True),
+        'complete_name': fields.function(
+            _name_get_fnc, type="char", string='Name'),
+        'parent_id': fields.many2one(
+            'product.type', 'Parent Type', select=True),
+        'child_id': fields.one2many(
+            'product.type', 'parent_id', string='Children Type'),
+        'sequence': fields.integer(
+            'Sequence',
+            help="Gives the sequence order when displaying a list of product Types."),
     }
+
 
 class product_template(osv.osv):
 
     _inherit = 'product.template'
 
     _columns = {
-        'type_id': fields.many2one('product.type','Type', ),
-        'segment_id': fields.many2one('product.segment','Segment', ),
+        'type_id': fields.many2one('product.type', 'Type', ),
+        'segment_id': fields.many2one('product.segment', 'Segment', ),
     }
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
