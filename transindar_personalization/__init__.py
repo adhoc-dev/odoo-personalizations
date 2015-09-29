06214901e5ff47c5e0435386d65bf4a0e3c13b3c
Nicolas Mac Rouillon <nmr@ingadhoc.com>
2015-09-29 10:18:41 -0300

    FIX__ init__.py

--- transindar_personalization/__init__.py
+++ transindar_personalization/__init__.py
@@ -4,8 +4,5 @@
 # directory
 ##############################################################################
 from . import product
-<<<<<<< HEAD
 from . import sale_order
-=======
 from . import wizard
->>>>>>> d2ec9e3a6ac6aad2fadc8608eaf20e341b4700c5
