152d6cb6c1fc97aa080655509ff310fbcd81ad2d
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-04-22 10:42:16 -0300

    FIX

--- transindar_personalization/sale_order.py
+++ transindar_personalization/sale_order.py
@@ -11,8 +11,11 @@ class sale_order_line(models.Model):
     _inherit = 'sale.order.line'
 
     supplier_code = fields.Char(
-        related='product_id.product_tmpl_id.supplier_code')
+        related='product_id.product_tmpl_id.supplier_code',
+        readonly=True)
     internal_code = fields.Char(
-        related='product_id.internal_code')
+        related='product_id.internal_code',
+        readonly=True)
     product_brand_id = fields.Many2one(
-        related='product_id.product_tmpl_id.product_brand_id')
+        related='product_id.product_tmpl_id.product_brand_id',
+        readonly=True)
