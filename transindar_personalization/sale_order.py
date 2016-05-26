b04c2c97f4a68dd4e1162cf220a653426acf9a6c
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-05-26 16:21:25 -0300

    IMP add additional description

--- transindar_personalization/sale_order.py
+++ transindar_personalization/sale_order.py
@@ -3,7 +3,7 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from openerp import fields, models
+from openerp import fields, models, api
 
 
 class sale_order_line(models.Model):
@@ -19,3 +19,18 @@ class sale_order_line(models.Model):
     product_brand_id = fields.Many2one(
         related='product_id.product_tmpl_id.product_brand_id',
         readonly=True)
+    additional_description = fields.Char(
+    )
+
+    @api.one
+    @api.onchange('additional_description')
+    def change_additional_description(self):
+        vals = self.product_id_change(
+            pricelist=self.order_id.pricelist_id.id,
+            product=self.product_id.id,
+            partner_id=self.order_id.partner_id.id)
+        name = vals.get('value', {}).get('name', False)
+        if name:
+            if self.additional_description:
+                name = "%s\n%s" % (name, self.additional_description or '')
+            self.name = name
